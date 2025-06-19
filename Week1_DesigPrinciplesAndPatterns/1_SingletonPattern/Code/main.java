521ddb689b65571611711054ef1280dd2e3426e4
Krupa Mohan <krupasam14@gmail.com>
2025-06-20 00:21:17 +0530

    Re-added Week1 folder cleanly after submodule cleanup

---
+++ Week1_DesigPrinciplesAndPatterns/1_SingletonPattern/Code/main.java
@@ -0,0 +1,9 @@
+public class main{
+    public static void main(String[] args){
+        Logger logger1 = Logger.getInstance();
+        Logger logger2 = Logger.getInstance();
+        logger1.log("First log message");
+        logger2.log("Second log message");
+        System.out.println("Are both logger instances same? "+(logger1==logger2));
+    }
+}
---
+++ Week1_DesigPrinciplesAndPatterns/2_FactoryMethodPattern/Code/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        DocumentFactory wordFactory = new WordDocumentFactory();
+        Document word = wordFactory.createDocument();
+
+        DocumentFactory pdfFactory = new PdfDocumentFactory();
+        Document pdf = pdfFactory.createDocument();
+        pdf.open();
+
+        DocumentFactory excelFactory = new ExcelDocumentFactory();
+        Document excel = excelFactory.createDocument();
+        excel.open();
+    }
+}
